163e1c1d3783bdbefb93bde0508420accddf6d06
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2015-05-19 17:41:51 +0200

    builders.d: build speex using cmake

--- submodules/build/builders.d/speex.mk
+++ submodules/build/builders.d/speex.mk
@@ -1,43 +1,69 @@
-#speex
-
-speex_dir=externals/speex
 
 
+############################################################################
+# Copyright (C) 2014  Belledonne Communications,Grenoble France
+#
+############################################################################
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+############################################################################
+enable_speex?=yes
+
+speex_project:=speex
+speex_project_dir:=externals
+speex_cmake_specific_options:=-DENABLE_STATIC=YES
 ifeq (,$(findstring i386,$(host)))
-	CFLAGS := $(CFLAGS) -marm
-	SPEEX_CONFIGURE_OPTION := --disable-float-api --enable-fixed-point
+	speex_cmake_specific_options += -ENABLE_FLOAT_API=NO -DENABLE_FIXED_POINT=YES
 endif
-
 ifneq (,$(findstring armv7,$(host)))
-	SPEEX_CONFIGURE_OPTION += --enable-armv7neon-asm --enable-arm5e-asm
+	speex_cmake_specific_options += -DENABLE_ARMV7_NEON_ASM=YES -DENABLE_ARM5E_ASM=YES
 endif
 ifneq (,$(findstring aarch64,$(host)))
-	SPEEX_CONFIGURE_OPTION += --enable-armv7neon-asm
+	speex_cmake_specific_options += -DENABLE_ARMV7_NEON_ASM=YES
 endif
 
-ifeq ($(enable_debug),yes)
-	CFLAGS := $(CFLAGS) -g
-	SPEEX_CONFIGURE_OPTION += --enable-debug
+speex_src_dir=$(BUILDER_SRC_DIR)/${speex_project_dir}/${speex_project}
+speex_build_dir=$(BUILDER_BUILD_DIR)/${speex_project_dir}/${speex_project}
+
+${speex_build_dir}/Makefile:
+	rm -rf ${speex_build_dir}/CMakeCache.txt ${speex_build_dir}/CMakeFiles/ && \
+	mkdir -p ${speex_build_dir} && \
+	cd ${speex_build_dir} && \
+	cmake -DCMAKE_TOOLCHAIN_FILE=$(BUILDER_SRC_DIR)/build/toolchain.cmake -DIOS_ARCH=${ARCH} \
+		-DCMAKE_INSTALL_PREFIX=$(prefix) -DCMAKE_PREFIX_PATH=$(prefix) -DCMAKE_MODULE_PATH=$(prefix)/share/cmake/Modules/ \
+		${speex_cmake_specific_options} ${speex_src_dir}
+
+ifeq ($(enable_${speex_project}),yes)
+build-${speex_project}: ${speex_build_dir}/Makefile
+	cd ${speex_build_dir} && \
+	make && \
+	make install
+else
+build-${speex_project}:
+	@echo "${speex_project} is disabled"
 endif
 
-$(BUILDER_SRC_DIR)/$(speex_dir)/configure:
-	 cd $(BUILDER_SRC_DIR)/$(speex_dir) && ./autogen.sh
 
-$(BUILDER_BUILD_DIR)/$(speex_dir)/Makefile: $(BUILDER_SRC_DIR)/$(speex_dir)/configure
-	mkdir -p $(BUILDER_BUILD_DIR)/$(speex_dir)
-	cd $(BUILDER_BUILD_DIR)/$(speex_dir)/\
-	&& CONFIG_SITE=$(BUILDER_SRC_DIR)/build/$(config_site) CFLAGS="$(CFLAGS) -O2" \
-	$(BUILDER_SRC_DIR)/$(speex_dir)/configure -prefix=$(prefix) --host=$(host) ${library_mode} --disable-ogg  $(SPEEX_CONFIGURE_OPTION)
+clean-${speex_project}:
+	cd $(speex_build_dir) && \
+	make clean
 
-build-speex: $(BUILDER_BUILD_DIR)/$(speex_dir)/Makefile
-	cd $(BUILDER_BUILD_DIR)/$(speex_dir) && make  && make install
+veryclean-${speex_project}:
+	if [ -d $(speex_build_dir) ]; then grep -v $(prefix) $(speex_build_dir)/install_manifest.txt | xargs rm; fi && \
+	rm -rf $(speex_build_dir)
 
-clean-speex:
-	cd  $(BUILDER_BUILD_DIR)/$(speex_dir)  && make clean
+clean-makefile-${speex_project}: veryclean-${speex_project}
 
-veryclean-speex:
-#	-cd $(BUILDER_BUILD_DIR)/$(speex_dir) && make distclean
-	-rm -f $(BUILDER_SRC_DIR)/$(speex_dir)/configure
-
-clean-makefile-speex:
-	cd $(BUILDER_BUILD_DIR)/$(speex_dir) && rm -f Makefile
